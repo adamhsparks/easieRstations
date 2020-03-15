a5c27f5e644decb9b47c1127ff89d2ae75ffae9c
Adam H. Sparks <adamhsparks@gmail.com>
2020-03-15 14:53:35 +1000

    Burn it all down! Use {golem}

---
+++ R/app_server.R
@@ -0,0 +1,113 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function(input, output, session) {
+  # List the first level callModules here
+
+  shiny::shinyServer(function(input, output, session) {
+    #Geodata
+    load(system.file("extdata", "isd_history.rda", package = "GSODR")) #nocov
+
+    stations <-
+      sf::st_as_sf(x = isd_history,
+                   coords = c("LON", "LAT"),
+                   crs = 4326)
+
+    # Start Shiny Server ---------------------------------------------------------
+    # show markers only on zoom and extract based on bounding box
+    options(shiny.sanitize.errors = F)
+    options(shiny.maxRequestSize = 30 * 1024 ^ 2)
+
+    # leaflet --------------------------------------------------------------------
+    output$mymap <- leaflet::renderLeaflet({
+      leaflet::leaflet() %>%
+        leaflet::addProviderTiles(providers$CartoDB.Positron) %>%
+        addDrawToolbar(
+          polylineOptions = F,
+          circleOptions = F,
+          markerOptions = F,
+          circleMarkerOptions = F,
+          polygonOptions = F
+        )
+    })
+
+    shiny::observeEvent(input$map_draw_new_feature, {
+      feat <- input$map_draw_new_feature
+      coords <- unlist(feat$geometry$coordinates)
+      coords <- matrix(coords, ncol = 2, byrow = T)
+      poly <-
+        sf::st_sf(st_sfc(st_polygon(list(coords))), crs = 4326)
+    })
+
+    #Substations <-
+    # sf::st_intersection(stations, poly)
+    #browser()
+
+    pop_up_text <- paste(
+      "ID: ",
+      paste0(stations$STNID),
+      "<br>",
+      "Name: ",
+      paste0(stations$NAME),
+      "<br>",
+      "Start Records: ",
+      paste0(stations$BEGIN),
+      "<br>",
+      "End Records: ",
+      paste0(stations$END),
+      "<br>"
+    )
+
+    leaflet::leafletProxy("mymap") %>%
+      leaflet::clearMarkers() %>%
+      leaflet::clearMarkerClusters() %>%
+      leaflet::addCircleMarkers(
+        data = stations["STNID"],
+        popup = pop_up_text,
+        stroke = FALSE,
+        radius = 5,
+        fillColor = "#bb0000",
+        fillOpacity = 0.5,
+        clusterOptions = markerClusterOptions()
+      )
+
+
+    # Handle Selector Based Events --------------------------------------------
+    TS <- shiny::eventReactive(input$data, {
+      ObtainTimeSeries(
+        StationID = input$StatID,
+        Start = input$Start,
+        End = input$End
+      )
+    })
+
+    shiny::observeEvent(input$data, {
+      if (is.data.frame(TS())) {
+        output$WeatherOUT <- DT::renderDataTable({
+          datatable(TS(), options = list(scrollX = T))
+        })
+      }
+    })
+
+    output$down.csv <- shiny::downloadHandler(
+      filename = function() {
+        paste0(
+          "Weather_Station",
+          input$StatID,
+          "_Start",
+          input$Start,
+          "_End",
+          input$End,
+          ".csv"
+        )
+      },
+
+      content = function(file) {
+        write_csv(TS(), file)
+      }
+    )
+  })
+}
---
+++ R/app_ui.R
@@ -0,0 +1,107 @@
+
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_ui <- function(request) {
+  tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
+    # List the first level UI elements here
+    shinydashboard::dashboardHeader(
+      # dashboard header -------------------------------------------------------
+      title = paste(
+        "easyGSODR v",
+        utils::packageVersion("easyGSODR"),
+        sep = "",
+        collapse = ""
+      ),
+      titleWidth = 300
+    ),
+
+    shinydashboard::dashboardSidebar(
+      # dashboard sidebar ------------------------------------------------------
+      shinydashboard::sidebarMenu(
+        id = "tab_menu",
+        shinydashboard::menuItem(
+          "Map",
+          tabName = "map",
+          icon = shiny::icon("map-marked-alt"),
+          selected = TRUE
+        ),
+        shinydashboard::menuItem(
+          "Download",
+          icon = shiny::icon("book-open"),
+          tabName = "download"
+        )
+      ),
+      width = 150
+    ),
+
+    shinydashboard::dashboardBody(
+      # content tab map viewer -------------------------------------------------
+      shinydashboard::tabItems(
+        shinydashboard::tabItem(tabName = "map",
+                                #h2("Dashboard tab content")
+                                shiny::fluidRow(
+                                  shinydashboard::box(
+                                    width = 12,
+                                    solidHeader = TRUE,
+                                    leaflet::leafletOutput("mymap",
+                                                           height = 1000)
+                                  )
+                                )),
+        shinydashboard::tabItem(
+          tabName = "download",
+          shiny::fluidRow(
+            shinydashboard::box(width = 3,
+                                shiny::textInput("StatID",
+                                                 "Station ID")),
+            shinydashboard::box(width = 3,
+                                shiny::numericInput("Start",
+                                                    "Start",
+                                                    value = 2000)),
+            shinydashboard::box(width = 3,
+                                shiny::numericInput("End",
+                                                    "End",
+                                                    value = 2018)),
+            shinydashboard::box(width = 3,
+                                shiny::actionButton("data",
+                                                    "Create Time-Series:"))
+          ),
+          shiny::fluidRow(DT::dataTableOutput("WeatherOUT")),
+          shiny::fluidRow(shinydashboard::box(
+            width = 3,
+            shiny::downloadButton("down.csv", "Download CSV")
+          ))
+        )
+      ),
+      shinybusy::add_busy_spinner()
+    ),
+
+    shinydashboard::dashboardPage(header,
+                                  sidebar,
+                                  body)
+  )
+}
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  add_resource_path('www', app_sys('app/www'))
+
+  tags$head(favicon(),
+            bundle_resources(path = app_sys('app/www'),
+                             app_title = 'easyGSODR')
+            # Add here other external resources
+            # for example, you can add shinyalert::useShinyalert()
+            )
+}
